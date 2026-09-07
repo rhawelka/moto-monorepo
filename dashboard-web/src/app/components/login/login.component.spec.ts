d72c84e1abfd5f078ede0b5306408e2c3f5de133
rhawelka <rhawelka@users.noreply.github.com>
2026-09-07 18:41:53 +0200

    test

--- dashboard-web/src/app/components/login/login.component.spec.ts
+++ dashboard-web/src/app/components/login/login.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,6 +9,7 @@ describe('LoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LoginComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
