c246b4889dcdb71c6fd30c88a2e9e6e1586ae0de
rhawelka <rhawelka@users.noreply.github.com>
2026-09-15 14:01:21 +0200

    test

--- shared/ui/src/lib/common-header/common-header.ts
+++ shared/ui/src/lib/common-header/common-header.ts
@@ -3,12 +3,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterModule } from '@angular/router';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'ui-common-header',
   templateUrl: './common-header.html',
   styleUrl: './common-header.scss',
-  imports: [RouterModule, MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    TranslocoDirective,
+  ],
 })
 export class CommonHeader {
   @Output() logout = new EventEmitter<void>();
